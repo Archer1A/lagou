976029ca61483b5d9f9ac43c851870f40d29992e
wangyijun <985695123@qq.com>
2018-04-25 23:26:50 +0800

    添加了将数据存储到EXCEL功能

--- lagou.py
+++ lagou.py
@@ -1,22 +1,68 @@
 #coding:utf-8
+import json
+
 import requests
+import time
+from xlwt  import *
+from lxml import etree
+COUNT = 0
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+    'Cookie': 'user_trace_token=20170922185141-04566935-9f84-11e7-a226-525400f775ce; LGUID=20170922185141-04566fdd-9f84-11e7-a226-525400f775ce; _ga=GA1.2.821244537.1506077500; JSESSIONID=ABAAABAACEBACDG0B2A07FB0B86DB2C9058587E9CF40FC3; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1524488209; LGSID=20180423205647-c8287626-46f5-11e8-99d3-525400f775ce; PRE_UTM=; PRE_HOST=www.baidu.com; PRE_SITE=https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3DQjrhpv1w1zbf5pebmPs_b2nAp4cDpqcZK3e89VPkUDm%26wd%3D%26eqid%3De0b759ed00087602000000045addd806; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; _gid=GA1.2.1371465779.1524488210; index_location_city=%E5%85%A8%E5%9B%BD; TG-TRACK-CODE=index_search; LGRID=20180423205704-d23dd02e-46f5-11e8-99d3-525400f775ce; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1524488227; SEARCH_ID=b9d50fb70301408aa872c6a7d8401a27',
+    'Referer': 'https://www.lagou.com/jobs/list_Python?px=default&city=%E5%85%A8%E5%9B%BD#filterBox',
 
+}
 def main():
-    headers = {
-        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-        'Cookie':'user_trace_token=20171119092250-7c52ac87-873e-477b-9614-a2b913f6b416; __guid=237742470.164011108602126720.1511054570975.4329; PRE_UTM=; PRE_HOST=; PRE_SITE=; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2Fzhaopin%2FPython%2F%3FlabelWords%3Dlabel; LGUID=20171119092255-2bb2e39b-ccc8-11e7-981f-525400f775ce; index_location_city=%E6%9D%AD%E5%B7%9E; JSESSIONID=ABAAABAACEBACDG31953364533185D090B2345CF77E4CD7; _gid=GA1.2.1445483945.1511054574; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1511054575,1511055179; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1511056132; _ga=GA1.2.1949819308.1511054574; LGSID=20171119092255-2bb2e1ba-ccc8-11e7-981f-525400f775ce; LGRID=20171119094852-cbb292aa-cccb-11e7-9823-525400f775ce; SEARCH_ID=0ff5f878f80548ab8fcd19de4c727287; monitor_count=15',
-        'Referer':'https://www.lagou.com/jobs/list_Python?px=default&city=%E5%85%A8%E5%9B%BD#filterBox',
+    global COUNT
+    file = Workbook(encoding='utf-8')
+    table = file.add_sheet('aaa')
 
-    }
-    form_date = {
-        'first':'true',
-        'pn':'1',
-        'kd':'Python'
-    }
 
-    url = 'https://www.lagou.com/jobs/positionAjax.json?px=default&needAddtionalResult=false&isSchoolJob=0'
-    result = requests.post(url=url,headers=headers,data=form_date)
-    print(result.content.decode("utf-8"))
+    for i in range(1,31):
+        form_date = {
+            'first':'true',
+            'pn':str(i),
+            'kd':"电商推广"
+        }
+
+        url = 'https://www.lagou.com/jobs/positionAjax.json?city=%E6%9D%AD%E5%B7%9E&needAddtionalResult=false'
+        result = requests.post(url=url,headers=headers,data=form_date)
+        print(result.text)
+        print("正在爬取第%d页"%(i))
+
+        js = json.loads(result.text)
+        try:
+            positions = js["content"]["positionResult"]["result"]
+            for position in positions:
+                #print(position)
+                table.write(COUNT,0,str(position["positionName"]))
+                table.write(COUNT,1,str(position["education"]))
+                table.write(COUNT,2,str(position["city"]))
+                table.write(COUNT,3,str(position["companyShortName"]))
+                table.write(COUNT,4,str(position["salary"]))
+                table.write(COUNT,5,str(position["positionAdvantage"]))
+                table.write(COUNT,6,str(position["companyLabelList"]))
+                table.write(COUNT,7,str(position["companyLabelList"]))
+                table.write(COUNT,8,getDetail(position["positionId"]))
+                print("==============正在爬取%s的职位描述=============="%(str(position["positionName"])))
+                COUNT += 1
+        except KeyError:
+            print("lllll")
+        time.sleep(4)
+
+    file.save("detail.xls")
+
+def  getDetail(id):
+    time.sleep(1)
+    url = "https://www.lagou.com/jobs/%s.html"%(id)
+    res = requests.get(url=url, headers = headers)
+    #print(res.text)
+    tree = etree.HTML(res.text)
+    content=tree.xpath('//*[@id="job_detail"]/dd[2]/div/*/text()')
+    return str(content)
+
+
 
 if __name__ == '__main__':
     main()
+    #getDetail(4451893)
--- test.py
+++ test.py
@@ -1,2 +1,59 @@
-#coding:utf-8
-#author:wangyijun
+# -*- coding: UTF-8 -*-
+import requests
+import xlwt
+headers = {      #头部分用来模拟浏览器，包括'User-Agent'，浏览器信息，'Referer'来源，'Cookie' 缓存信息
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+    'Referer':'https://www.lagou.com/jobs/list_python?labelWords=&fromSearch=true&suginput=',
+    'Cookie':'user_trace_token=20170926134508-daf8d2e1-a27d-11e7-ac91-525400f775ce; LGUID=20170926134508-daf8d7b1-a27d-11e7-ac91-525400f775ce; index_location_city=%E5%85%A8%E5%9B%BD; JSESSIONID=ABAAABAAAGGABCB0B1EC33C40CD9ED0ACD99A669777BD37; TG-TRACK-CODE=search_code; _gat=1; _gid=GA1.2.1406285458.1506404713; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1506404708,1506479284,1506479293; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1506481150; _ga=GA1.2.583885505.1506404708; LGSID=20170927102811-81f3f0f9-a32b-11e7-aefd-525400f775ce; LGRID=20170927105908-d4f5f541-a32f-11e7-92ef-5254005c3644; SEARCH_ID=2c80705036554d82a34a71475dfae529'
+
+}
+def getJobList(page):
+    data={
+        'first': 'false',
+        'pn': page,
+        'kd':'python'
+    }
+    #post请求的三个变量 url，data信息，headers信息
+    res = requests.post('https://www.lagou.com/jobs/positionAjax.json?needAddtionalResult=false&isSchoolJob=0',data=data,headers=headers)
+    result=res.json()
+    jobs = result['content']['positionResult']['result']  #结合浏览器查看网页的结构
+    return jobs
+    # print(res.text)
+
+#创建excel表
+excel1 = xlwt.Workbook()
+sheet1 = excel1.add_sheet('la',cell_overwrite_ok=True) #设置单元格属性
+
+
+#需要爬取的关键字，设置excel表头
+sheet1.write(0, 0, 'companyFullName')
+sheet1.write(0, 1, 'firstType')
+sheet1.write(0, 2, 'positionName')
+sheet1.write(0, 3, 'education')
+sheet1.write(0, 4, 'financeStage')
+sheet1.write(0, 5, 'city')
+sheet1.write(0, 6, 'district')
+sheet1.write(0, 7, 'industryField')
+
+#循环遍历翻页
+n = 1
+for page in range(1,10):
+        print (page)  #打印页码
+        for job in getJobList(page):
+            # print (page)  #打印每页的内容
+            print(job)
+            #将想要获取的内容写入到excel
+            sheet1.write(n, 0, job['companyFullName'])
+            sheet1.write(n, 1, job['firstType'])
+            sheet1.write(n, 2, job['positionName'])
+            sheet1.write(n, 3, job['education'])
+            sheet1.write(n, 4, job['financeStage'])
+            sheet1.write(n, 5, job['city'])
+            sheet1.write(n, 6, job['district'])
+            sheet1.write(n, 7, job['industryField'])
+            n+=1
+        import time
+        # time.sleep(2)
+    #print (job['companyFullName'])
+#将excel表保存到桌面
+excel1.save('lagou.xls')
